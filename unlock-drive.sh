a0b5b3dfe83bb891df2e86519e5b480067aaa10e
Bart Massey <bart@cs.pdx.edu>
2015-05-26 20:59:22 -0700

    added (removable) password check to unlock-drive

--- unlock-drive.sh
+++ unlock-drive.sh
@@ -16,6 +16,14 @@ then
     exit 0
 fi
 
+CHECK=true
+
+case $# in
+    -n)
+        CHECK=false
+        shift
+        ;;
+esac
 case $# in
     0)
         RDRIVE="`locate-drive backup`"
@@ -29,10 +37,21 @@ case $# in
         RDRIVE="$1"
         ;;
     *)
-        echo "unlock-drive: usage: unlock-drive [<dev>]" >&2
+        echo "unlock-drive: usage: unlock-drive [-n] [<dev>]" >&2
         exit 1
         ;;
 esac
 
 sudo cryptsetup create -c aes -s 128 backup "$RDRIVE" </dev/tty &&
+if $CHECK
+then
+    if mount-drive
+    then
+        umount-drive
+    else
+        lock-drive
+        echo "test mount failed: probably bad password" >&2
+        exit 1
+    fi
+fi &&
 echo $MDRIVE
